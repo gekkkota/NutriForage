f781700ef06e34393834d699d5cbaab054a35d3c
sumampongaldrin <sumampong3@gmail.com>
2023-01-14 15:38:48 +0800

    Splash Screen, Too enlarged

--- app/src/main/java/com/example/nutriforagepractice/MainActivity.java
+++ app/src/main/java/com/example/nutriforagepractice/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_NutriForagePractice);
         setContentView(R.layout.activity_main);
 
         text = findViewById(R.id.user_details);
