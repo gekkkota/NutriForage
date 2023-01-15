2ea13926771e28b42bc2a8546018244359788c67
sumampongaldrin <sumampong3@gmail.com>
2023-01-15 21:19:30 +0800

    Navigation Bar

--- app/src/main/java/com/example/nutriforagepractice/UserDashboard.java
+++ app/src/main/java/com/example/nutriforagepractice/UserDashboard.java
@@ -1,15 +1,65 @@
 package com.example.nutriforagepractice;
 
+import android.content.Intent;
+import android.graphics.Camera;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class UserDashboard extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_dashboard);
+
+        navigationView = findViewById(R.id.bottom_navigation);
+
+
+        navigationView.setSelectedItemId(R.id.nav_home);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()){
+
+                    case R.id.nav_home:
+                        startActivity(new Intent(getApplicationContext(), UserDashboard.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.nav_log:
+                        fragment = new LogFragment();
+                        break;
+
+                    case R.id.nav_camera:
+                        fragment = new CameraFragment();
+                        break;
+
+                    case R.id.nav_list:
+                        fragment = new ListFragment();
+                        break;
+
+                    case R.id.nav_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_container, fragment).commit();
+                return true;
+            }
+
+        });
     }
 }
