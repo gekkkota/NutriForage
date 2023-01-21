4e54f3ce44f931fa710f45a3d10087a9432125ec
gekkkota <cervantesandrei@gmail.com>
2023-01-21 16:32:52 +0800

    Added Users File

---
+++ app/src/main/java/com/example/nutriforagepractice/Users.java
@@ -0,0 +1,18 @@
+package com.example.nutriforagepractice;
+
+public class Users {
+
+    public String fullname, email, age, height, weight;
+
+    public Users(){
+
+    }
+
+    public Users(String fullname, String email, String age, String height, String weight){
+        this.fullname = fullname;
+        this.email = email;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+}
